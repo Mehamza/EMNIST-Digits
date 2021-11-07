2c0c7d2ec587e6557d8d476320f18ff545c0956d
Mohamed Hamza <46716114+Mehamza@users.noreply.github.com>
2021-11-07 12:57:00 +0100

    Create generating_predictions.py

---
+++ generating_predictions.py
@@ -0,0 +1,30 @@
+# import packages 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# A few random samples
+use_samples = [5, 38, 3939, 27389]
+
+# Generate plots for samples
+for sample in use_samples:
+  # Generate a plot
+  reshaped_image = input_train[sample].reshape((img_width, img_height))
+  plt.imshow(reshaped_image)
+  plt.show()
+  
+# A few random samples
+use_samples = [5, 38, 3939, 27389]
+samples_to_predict = []
+
+# Generate plots for samples
+for sample in use_samples:
+  # Generate a plot
+  reshaped_image = input_train[sample].reshape((img_width, img_height))
+  plt.imshow(reshaped_image)
+  plt.show()
+  # Add sample to array for prediction
+  samples_to_predict.append(input_train[sample])
+  
+  # Convert into Numpy array
+samples_to_predict = np.array(samples_to_predict)
+print(samples_to_predict.shape)
