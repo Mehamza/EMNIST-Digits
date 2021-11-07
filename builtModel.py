73aa04f635994e8df7b3be05cdcd93891557a7ea
Mehamza <46716114+Mehamza@users.noreply.github.com>
2021-11-07 04:56:20 +0100

    Update builtModel.py

--- builtModel.py
+++ builtModel.py
@@ -1,1 +1,7 @@
-
+#import packages :
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+from extra_keras_datasets import emnist
