3bc48492f13a1169243b35a4319aea9e9eace923
Mohamed Hamza <46716114+Mehamza@users.noreply.github.com>
2021-11-07 12:46:20 +0100

    Create Saving_loading_model.py

---
+++ Saving_loading_model.py
@@ -0,0 +1,10 @@
+#import packages 
+from tensorflow.keras.models import Sequential, save_model, load_model
+
+# Save the model
+filepath = './saved_model'
+save_model(model, filepath)
+
+# Save the model
+filepath = './saved_model'
+save_model(model, filepath)
